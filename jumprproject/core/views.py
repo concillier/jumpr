b34c0113e1bae0dbce574864a25b02d8a7cfefc6
Concillier <concillier.data@gmail.com>
2023-05-23 11:41:59 +0200

    Not sure

--- jumprproject/core/views.py
+++ jumprproject/core/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 
+import requests
+import json
+
 # Create your views here.
 
 def home(request): 
-    return render(request, 'home.html')
+
+    url = 'https://api.open-meteo.com/v1/forecast?latitude=55.68&longitude=12.57&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,sunrise,sunset&timezone=Europe%2FBerlin'
+
+    city = 'Copenhagen'
+
+ #2. Pull & convert the data into json
+
+    weather = requests.get(url).json()
+
+    return render(request, 'home.html', {
+        'weather':weather
+    })
